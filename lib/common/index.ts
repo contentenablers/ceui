c1bead130996180ba688294c53674d3e4d71cebf
Anand Vimalanathan <tomeanand@gmail.com>
2024-06-24 12:32:36 +0530

    feat: Removed components

--- lib/common/index.ts
+++ lib/common/index.ts
@@ -1,3 +1,3 @@
 export { default as Button} from './Button'
 export { default as Card} from './Card'
-export { default as PullDownButton} from './PullDownButton'
+// export { default as PullDownButton} from './PullDownButton'
