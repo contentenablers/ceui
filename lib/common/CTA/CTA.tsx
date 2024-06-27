a006afa7e553b1a5e6bf1626edbd4d67d9ad0ff2
Anand Vimalanathan <tomeanand@gmail.com>
2024-06-27 13:27:33 +0530

    feat: Buttons changed
    Buttons

--- lib/common/CTA/CTA.tsx
+++ lib/common/CTA/CTA.tsx
@@ -3,7 +3,7 @@ import { Button } from '@headlessui/react'
 function CTA() {
   return (
     <Button className="rounded bg-sky-600 py-2 px-4 text-sm text-white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">
-      Save changes ::
+      Save changes
     </Button>
   )
 }
